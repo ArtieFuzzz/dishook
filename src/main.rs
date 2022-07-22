23e3d30c00b530fce62bc9551271cf0d5f69640e
♥ ArtieFuzzz ♥ <48877462+ArtieFuzzz@users.noreply.github.com>
2022-07-22 12:55:25 +1000

    Handle Rejections

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use reqwest::Client;
 use std::{collections::HashMap, error::Error};
 use tracing::{info, warn};
 use tracing_subscriber;
-use warp::Filter;
+use warp::{Filter, http::StatusCode, reject::MethodNotAllowed};
 
 pub static HTTP: Lazy<Client> = Lazy::new(Client::new);
 
@@ -16,7 +16,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let hook = warp::post()
         .and(warp::path!("message"))
         .and(warp::body::json::<HashMap<String, serde_json::Value>>())
-        .and_then(routes::message);
+        .and_then(routes::message)
+        .recover(handle_rejection);
 
     info!("Starting...");
 
@@ -31,4 +32,15 @@ async fn main() -> Result<(), Box<dyn Error>> {
     warp::serve(hook).run(([127, 0, 0, 1], 80)).await;
 
     Ok(())
+}
+
+async fn handle_rejection(err: warp::Rejection) -> Result<impl warp::Reply, std::convert::Infallible> {
+  if err.is_not_found() {
+    Ok(warp::reply::with_status("Could not find that route", StatusCode::NOT_FOUND))
+  } else if let Some(m) = err.find::<MethodNotAllowed>() {
+    Ok(warp::reply::with_status("METHOD_NOT_ALLOWED", StatusCode::METHOD_NOT_ALLOWED))
+  } else {
+    eprintln!("Unhandled rejection: {:?}", err);
+    Ok(warp::reply::with_status("INTERNAL_SERVER_ERROR", StatusCode::INTERNAL_SERVER_ERROR))
+  }
 }
